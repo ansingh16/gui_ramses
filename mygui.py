8369309791561ed4862974877596608fda551467
Ankit Singh <ansingh16@gmail.com>
2016-11-13 13:28:09 +0530

    colorbar added

--- mygui.py
+++ mygui.py
@@ -31,19 +31,82 @@ class Window(QtGui.QMainWindow):
 
         extractAction = QtGui.QAction( QtGui.QIcon('todachoppa.png'),'Flee the scene',self)
         extractAction.triggered.connect(self.close_application)
-        
-
         self.toolBar = self.addToolBar("Extraction")
-
         self.toolBar.addAction(extractAction)
         
+
+        fontChoice = QtGui.QAction('Font',self)
+        fontChoice.triggered.connect(self.font_choice)
+        #self.toolBar = self.addToolBar("Fonts")
+        self.toolBar.addAction(fontChoice)
+        
+        
+        color = QtGui.QColor(0,0,0)
+        fontcolor = QtGui.QAction('Font bg color',self)
+        fontcolor.triggered.connect(self.color_picker)
+
+        self.toolBar.addAction(fontcolor)
+        
+
+
         checkBox = QtGui.QCheckBox('Enlarge window',self)
-        checkBox.move(100,25)
+        checkBox.move(300,25)
         checkBox.stateChanged.connect(self.enlarge_window)
 
-      
+        self.progress = QtGui.QProgressBar(self)
+        self.progress.setGeometry(200,80,250,20)
         
+        self.btn = QtGui.QPushButton("Downlod",self)
+        self.btn.move(200,120)
+        self.btn.clicked.connect(self.download)
+        
+        print(self.style().objectName())
+        self.styleChoice = QtGui.QLabel("gtk+",self)
+        
+        combobox=QtGui.QComboBox(self)
+        combobox.addItem("gtk+")
+        combobox.addItem("motif")
+        combobox.addItem("cde")
+        combobox.addItem("Plastique")
+
+        combobox.move(50,250)
+        self.styleChoice.move(50,150)
+
+        combobox.activated[str].connect(self.style_choice)
+        
+
+        cal = QtGui.QCalendarWidget(self)
+        cal.move(500,200)
+        cal.resize(200,200)
+
+
+
         self.show()
+
+    def color_picker(self):
+        color = QtGui.QColorDialog.getColor()
+        self.styleChoice.setStyleSheet("QWidget {background-color = %s }"% color.name())
+
+
+    def font_choice(self):
+        font, valid = QtGui.QFontDialog.getFont()
+        if valid:
+            self.styleChoice.setFont(font)
+
+
+    def style_choice(self,text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
+
+
+
+    def download(self):
+        self.completed = 0
+
+        while self.completed < 100 :
+            self.completed += 0.0001
+            self.progress.setValue(self.completed)
+            
 
     def enlarge_window(self,state):
         if state == QtCore.Qt.Checked:
